676424dbb82a6cf6bab453f54d29ef627bd77e16
Brodie Blackburn <brodie.blackburn@gmail.com>
2024-05-23 09:45:51 +1000

    Re-implement ubank module
    - I had another go at reverse engineering the ubank mobile app.
    - ubank.Client subclasses httpx.Client.
    - Device dataclass represents enrolled device.
    - --verbose flag enables httpx INFO logging.
    - Document new module.
    - Add changelog.

--- README.md
+++ README.md
@@ -1,92 +1,114 @@
 # ubank
 
-Access [ubank](https://www.ubank.com.au) programatically.
-
-This does not provide true API-like interface, but you can retrieve information
-using browser automation.
-
-
-## Requirements
-
-Python 3.8+ and [Playwright](https://playwright.dev/python/).
-
-
-## Installation
-
-Install from PyPI:
-
-```console
-$ pip install ubank
-```
-
-Install version of Firefox required by Playwright:
-
-```console
-$ playwright install --with-deps firefox
-```
+Access [ubank](https://www.ubank.com.au)'s API.
 
 
 ## Getting started
 
-Create an instance of `UbankClient` and log in using a security code:
-
-```python
->>> from ubank import UbankClient
->>> ubank_client = UbankClient()
->>> ubank_client.log_in_with_security_code('name@domain.com', 'SecretPassw0rd')
-Enter security code: 123456
-```
-
-Then you can get account information:
-
-```python
->>> ubank_client.get_accounts()
-{'linkedBanks': [{'bankId': 1, 'shortBankName': 'ubank', 'accounts': [{'label': 'Spend account', 'nickname': 'Spend account', 'type': 'TRANSACTION', 'balance': {'currency': 'AUD', 'current': 1000, 'available': 1000}, 'status': 'Active'}, {'label': 'Save account', 'nickname': 'Save account', 'type': 'SAVINGS', 'balance': {'currency': 'AUD', 'current': 10000, 'available': 10000}, 'status': 'Active'}]}]}
-```
-
-After logging in with a security code, you can retrieve a trusted cookie:
-
-```python
->>> ubank_client.get_trusted_cookie()
-{'name': '026d9560-3c86-4680-b926-44bdd28eba94', 'value': 'YmxhaCBibGFoIGJsYWggYmxhaCBibGFoIGJsYWggYmxhaCBibGFo', 'domain': 'www.ubank.com.au', 'path': '/', 'expires': 1706758407, 'httpOnly': True, 'secure': True, 'sameSite': 'Strict'}
-```
-
-Use the cookie to log in without a security code:
-
-```python
->>> ubank_client.log_in_with_trusted_cookie(
-...     'name@domain.com',
-...     'SecretPassw0rd',
-...     {'name': '026d9560-3c86-4680-b926-44bdd28eba94', 'value': 'YmxhaCBibGFoIGJsYWggYmxhaCBibGFoIGJsYWggYmxhaCBibGFo', 'domain': 'www.ubank.com.au', 'path': '/', 'expires': 1706758407, 'httpOnly': True, 'secure': True, 'sameSite': 'Strict'}
-... )
-```
-
-Stop Playwright gracefully when you're done:
-
-```python
->>> ubank_client.stop()
-```
-
-You can also retrieve a trusted cookie by running the ubank module from the command
-line. Use an environment variable to avoid storing your banking password in shell
-history:
-
+Install ubank with pip (Python 3.8+ is required):
 ```console
-$ read -s PASSWORD
-SecretPassw0rd
+$ pip install ubank
 ```
 
-Running ubank as a module will prompt for a security code and then display the trusted
-cookie object:
-
+Before accessing the API, you'll first need to enrol a new device with ubank.
+Running ubank as a module helps with this task:
 ```console
-$ python -m ubank name@domain.com "$PASSWORD"
-Enter security code: 123456
-{'name': '026d9560-3c86-4680-b926-44bdd28eba94', 'value': 'YmxhaCBibGFoIGJsYWggYmxhaCBibGFoIGJsYWggYmxhaCBibGFo', 'domain': 'www.ubank.com.au', 'path': '/', 'expires': 1706758407, 'httpOnly': True, 'secure': True, 'sameSite': 'Strict'}
+$ python -m ubank --help
+usage: ubank.py [-h] [-o FILE] [-v] username
+
+Enrols new device with ubank. You will be asked for your ubank password and secret code interactively.
+
+positional arguments:
+  username              ubank username
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -o FILE, --output FILE
+                        write JSON device credentials to file (default: write to stdout)
+  -v, --verbose         displays httpx INFO logs
 ```
 
-Secure storage of your username, password and trusted cookie is **your**
-responsibility.
+We'll enrol a new device and save the credentials to `device.json`.
+Keep this file safe!
+You'll be prompted for your ubank password and security code during this step.
+```console
+$  python -m ubank name@domain.com --output device.json
+Enter ubank password:
+Enter security code sent to 04xxxxx789: 123456
+$ cat device.json
+{
+  "hardware_id": "35bd47b0-eced-4fb4-88e1-24657c2500ec",
+  "device_id": "cc1d3291-8e7d-45fc-845b-326b65bffcb1",
+  "device_meta": "{\"appVersion\": \"15.11.1\", \"binaryVersion\": \"15.11.1\", \"deviceName\": \"iPhone19-1\", \"environment\": \"production\", \"instance\": \"live\", \"native\": true, \"platform\": \"ios\"}",
+  "hashed_pin": "N0ZsiU81f+qiOZvs424E06AasHBlHsSlH9Fj1J0Sz5c=",
+  "secret": "c3e59465-2449-4692-8d0a-6dc9bb8b2ae2",
+  "auth_key": "pLzKjKs0FW104tqaj5qD3wYmZf0Q+udRCsRgST1gRGwh9iaxVf5qZdn+LtidvqSx20Y=",
+  "email": "name@domain.com",
+  "mobile_number": "+61423456789",
+  "user_id": "51457aec-9fb4-45c4-9ed0-4d17b70665ec",
+  "username": "48b16c6f-19a5-46e7-855e-5d6922882276",
+  "token": "dw2FYNdTRLgIS8YxZlQ0RnihkpgxRB/+a/o3vmQWWiRtrF11H4ZjA8ywZfaoUYK/Gkc="
+}
+```
+
+
+## Accessing ubank's API
+
+You won't use your username and password to access ubank's API.
+Instead, you'll use the enrolled device's credentials (stored in `device.json`).
+
+Instantiate a `ubank.Device` from `device.json`:
+```python
+import ubank
+
+with open("device.json") as file:
+    device = ubank.Device(**json.load(file))
+```
+
+Next, we'll instantiate `ubank.Client` with the `device` created above.
+Use this class as a context manager.
+This ensures ubank sessions and HTTP connections are properly cleaned when leaving
+the `with` block.
+
+`ubank.Client`'s `base_url` is set to `https://api.ubank.com.au/`, so only the API path is required when making requests.
+
+> [!IMPORTANT]
+> You **must** store the instance's `.device` attribute after instantiation.
+> Otherwise the stored device credentials will be expired and you'll need to re-enrol.
+>
+> Instantiating `ubank.Client` refreshes the `auth_key` and long life `token`, held in the `.device` attribute.
+
+```python
+with ubank.Client(device) as client:
+    with open("device.json", "w") as file:
+        file.write(client.device.dumps())
+    print(client.get("/app/v1/accounts/summary").json())
+
+{'linkedBanks': [{'bankId': 1, 'shortBankName': 'ubank', 'accounts': [{'label': 'Spend', 'type': 'TRANSACTION', 'balance': {'currency': 'AUD', 'current': 100, 'available': 100}, 'status': 'Active', 'id': '695db516-b0e2-4807-baca-77314a6257ce', 'nickname': 'Spend', 'number': '12345678', 'bsb': '670864', 'lastBalanceRefresh': '2024-01-02T00:00:00.000Z', 'openDate': '2024-01-01T00:00:00.000Z', 'isJointAccount': False}, {'label': 'Save', 'type': 'SAVINGS', 'balance': {'currency': 'AUD', 'current': 1200.44, 'available': 1200.44}, 'status': 'Active', 'id': '5bad6edf-247e-4221-9bfc-e7608f5984cb', 'nickname': 'Save', 'number': '23456789', 'bsb': '670864', 'lastBalanceRefresh': '2024-01-02T00:00:00.000Z', 'openDate': '2024-01-01T00:00:00.000Z', 'isJointAccount': False}]}]}
+```
+
+
+## API endpoints
+
+Here are some API endpoints to try:
+```python
+with ubank.Client(device) as client:
+    with open("device.json", "w") as file:
+        file.write(client.device.dumps())
+    print(client.get("/app/v1/accounts").json())
+    print(client.get("/app/v1/accounts/summary").json())
+    print(client.get("/app/v1/achievements").json())
+    print(client.get("/app/v1/campaigns").json())
+    print(client.get("/app/v1/cards").json())
+    print(client.get("/app/v1/contacts").json())
+    print(client.get("/app/v1/customer-details").json())
+    print(client.get("/app/v1/insights").json())
+    print(client.get("/app/v1/insights/interest").json())
+    print(client.get("/app/v1/products").json())
+    print(client.get("/app/v1/promotions").json())
+    print(client.get("/app/v1/savings-goals").json())
+    print(client.get("/app/v1/tfn").json())
+```
 
 
 ## Release
@@ -104,3 +126,17 @@ Publish to PyPI:
 $ read -s PASSWORD
 $ poetry publish --build -u __token__ -p "$PASSWORD"
 ```
+
+
+## Changelog
+
+### 1.0.0
+
+- Drop Playwright requirement.
+- Re-implement with simpler and lightweight [httpx](https://www.python-httpx.org) libary.
+- Easier access to full ubank API.
+
+
+### 0.1.2
+
+- Automate ubank access using [Playwright](https://playwright.dev) headless browser.
--- ubank.py
+++ ubank.py
@@ -1,108 +1,304 @@
 import argparse
+import base64
+import json
+import logging
+import secrets
+import uuid
+from dataclasses import asdict, dataclass
+from getpass import getpass
 
-from playwright._impl._api_structures import Cookie
-from playwright.sync_api import sync_playwright
+import httpx
+
+# Unchanging headers in every request.
+base_headers = {
+    "Origin": "ionic://bank86400",
+    "x-private-api-key": "ANZf5WgzmVLmTUwAQyuCq7LspXF2pd4N",
+}
 
 
-class UbankClient:
-    def __init__(self, headless=True) -> None:
-        """Initialises Playwright browser, context and page objects.
+@dataclass
+class Device:
+    """Represents an enrolled ubank device."""
 
-        The Playwright browser is launched in headless mode by default.
+    hardware_id: str
+    device_id: str
+    device_meta: str
+    hashed_pin: str
+    secret: str
+    auth_key: str
+    email: str
+    mobile_number: str
+    user_id: str
+    username: str
+    token: str
+
+    def dumps(self) -> str:
+        """Returns JSON string representation of self."""
+        return json.dumps(asdict(self), indent=2)
+
+
+class Client(httpx.Client):
+    """ubank API client based on httpx.Client.
+
+    Initialise `Client` with a `Device` (see [`enrol_device()`](enrol_device)).
+    It's recommended to use this class as a context manager. This ensures ubank
+    sessions and HTTP connections are properly cleaned when leaving the `with` block:
+    ```python
+    with ubank.Client(device) as client:
+        ...
+    ```
+
+    Important! You **must** store the instance's `.device` attribute after
+    instantiation. Otherwise the stored device credentials will be expired and you'll
+    need to re-enrol.
+
+    Instantiating `ubank.Client` refreshes the `auth_key` and long life `token`,
+    held in the `.device` attribute.
+
+    `base_url` is set to https://api.ubank.com.au/, so only the API path is required
+    when making requests:
+    ```python
+    client.get("/app/v1/accounts/summary")
+    ```
+    """
+
+    def __init__(self, device: Device) -> None:
+        """Initialises ubank session using device credentials.
+
+        Initialisation sets `self.device` containing an updated auth key and token.
+        Be sure to save the updated device!
+
+        :param device: A `Device` enrolled with ubank
         """
-        self.playwright = sync_playwright().start()
-        # ubank doesn't play nice with Chromium: use Firefox.
-        self.browser = self.playwright.firefox.launch(headless=headless)
-        self.context = self.browser.new_context()
-        self.page = self.context.new_page()
+        with httpx.Client(
+            headers={
+                **base_headers,
+                "x-hardware-id": device.hardware_id,
+                "x-device-id": device.device_id,
+                "x-device-meta": device.device_meta,
+            },
+        ) as client:
+            # Authenticate with long life token.
+            response = client.post(
+                "https://api.ubank.com.au/app/v1/long-life-token/login",
+                json={
+                    "authKey": device.auth_key,
+                    "deviceUuid": device.device_id,
+                    "secret": device.secret,
+                    "token": device.token,
+                    "username": device.username,
+                },
+            )
+            # Set access and auth token headers for future requests.
+            self.access_token = client.headers["x-access-token"] = client.headers[
+                "x-auth-token"
+            ] = response.json()["accessToken"]
+            # Store other tokens in order to kill session in future.
+            self.refresh_token = response.json()["refreshToken"]
+            self.session_token = response.json()["sessionToken"]
+            # Update device's auth key.
+            device.auth_key = response.json()["authKey"]
 
-    def stop(self):
-        """Stops Playwright gracefully."""
-        self.context.close()
-        self.browser.close()
-        self.playwright.stop()
+            # Associate session with new auth key.
+            client.patch(
+                url="https://api.ubank.com.au/app/v1/sessions",
+                # Auth key UUID comes from previous response.
+                json={"authKeyUuid": response.json()["authKeyUuid"]},
+            )
 
-    def __enter__(self):
-        return self
+            # Refresh long life token.
+            response = client.post(
+                "https://api.ubank.com.au/app/v1/long-life-token/refresh",
+                json={"token": device.token},
+            )
+            # Update device token.
+            device.token = response.json()["token"]
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.stop()
+        # Maintain reference to updated device on this instance.
+        self.device = device
 
-    def _log_in(self, username: str, password: str) -> None:
-        """Performs username and password steps of log in flow."""
-        self.page.goto("https://www.ubank.com.au/welcome/login/username")
-        self.page.get_by_label("What's your username?").click()
-        self.page.get_by_label("What's your username?").fill(username)
-        self.page.get_by_label("What's your username?").press("Enter")
-        self.page.get_by_label("What's your password?").click()
-        self.page.get_by_label("What's your password?").fill(password)
-        self.page.get_by_label("What's your password?").press("Enter")
-
-    def log_in_with_security_code(self, username: str, password: str) -> None:
-        """Logs in with username, password and security code (interactive prompt)."""
-        self._log_in(username, password)
-        # Trust browser and prompt for security code interactively.
-        self.page.locator("label").filter(has_text="Yes").click()
-        self.page.get_by_role("button", name="Next").click()
-        self.page.get_by_label("Enter security code").click()
-        self.page.get_by_label("Enter security code").fill(
-            input("Enter security code: ")
+        # Initialise new httpx.Client, keeping headers and cookies from client.
+        super().__init__(
+            headers=client.headers,
+            cookies=client.cookies,
+            base_url="https://api.ubank.com.au/",
         )
-        self.page.get_by_label("Enter security code").press("Enter")
-        self.page.wait_for_url("https://www.ubank.com.au/welcome/my/accounts")
 
-    def log_in_with_trusted_cookie(
-        self, username: str, password: str, cookie: Cookie
-    ) -> None:
-        """Logs in with username, password and trusted browser cookie."""
-        # Add trusted browser cookie into browser context.
-        self.context.add_cookies([cookie])  # type: ignore
-        self._log_in(username, password)
-        self.page.wait_for_url("https://www.ubank.com.au/welcome/my/accounts")
+    def _delete_session(self) -> None:
+        """Kills ubank session."""
+        self.request(
+            "DELETE",
+            "/app/v1/sessions",
+            json={
+                "accessToken": self.access_token,
+                "refreshToken": self.refresh_token,
+                "sessionToken": self.session_token,
+            },
+        )
 
-    def get_trusted_cookie(self) -> Cookie:
-        """Returns trusted cookie from authenticated session.
+    def close(self) -> None:
+        """Kills ubank session before closing."""
+        self._delete_session()
+        super().close()
 
-        When you log in to ubank you can optionally trust the browser. This sets a cookie
-        so that you don't need to perform security code verification each time you log
-        in.
-        """
-        # Extract trusted cookie by name from browser context. It's possible the
-        # cookie name differs across accounts. If so, the cookie could be identified
-        # by matching its name to a pattern.
-        trusted_cookie = [
-            cookie
-            for cookie in self.context.cookies()
-            if cookie["name"] == "70484507-60ac-4c04-afac-b84c9c85e504"  # type: ignore
-        ][0]
-        return trusted_cookie
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        """Kills ubank session before exiting the context."""
+        self._delete_session()
+        super().__exit__(exc_type, exc_value, traceback)
 
-    def get_accounts(self) -> dict:
-        """Returns response from /app/v1/accounts.
 
-        Calls to the ubank API use heavily obfuscated Javascript. It's not clear
-        how to construct the required request headers.
+def enrol_device(
+    username: str, password: str, app_version="11.21.1", device_name="iPhone16-1"
+) -> Device:
+    """Enrols new device with ubank.
 
-        Simply navigating to some pages kicks off API requests in the background.
-        This method navigates to the account overview page, waits for an API request
-        to the /accounts endpoint, and then returns the JSON response object.
-        """
-        with self.page.expect_request_finished(
-            lambda request: request.method == "GET"
-            and request.url.startswith("https://www.ubank.com.au/app/v1/accounts")
-        ) as event:
-            self.page.goto("https://www.ubank.com.au/welcome/my/accounts")
-        return event.value.response().json()
+    You are responsible for securely storing the enrolled device's information returned
+    by this function.
+
+    :param username: ubank username
+    :param password: ubank password
+    :param app_version: Set ubank application version identifier. See versions here:
+        https://apps.apple.com/au/app/id1449543099.
+    :param device_name: Set device identifier. See "Hardware strings" row in this
+        table: https://en.wikipedia.org/wiki/List_of_iPhone_models. Replace comma
+        with hyphen.
+    :return: Enrolled device credentials
+    """
+    # Generate a fresh hardware ID.
+    hardware_id = str(uuid.uuid4())
+    # Start with an empty device ID. ubank will assign one later.
+    device_id = ""
+    # Build device meta string from app version and device name.
+    device_meta = json.dumps(
+        {
+            "appVersion": app_version,
+            "binaryVersion": app_version,
+            "deviceName": device_name,
+            "environment": "production",
+            "instance": "live",
+            "native": True,
+            "platform": "ios",
+        }
+    )
+
+    with httpx.Client(
+        headers={
+            **base_headers,
+            "x-hardware-id": hardware_id,
+            "x-device-id": device_id,
+            "x-device-meta": device_meta,
+        }
+    ) as client:
+        # Start enrolment by identifying ourselves.
+        response = client.post(
+            url="https://api.ubank.com.au/app/v1/welcome",
+            json={"identity": username},
+        )
+
+        # Next, authenticate with password.
+        response = client.post(
+            url="https://api.ubank.com.au/app/v1/challenge/password",
+            json={"deviceName": device_name, "password": password},
+            # Set access and auth token headers from previous response.
+            headers={
+                "x-access-token": response.json()["accessToken"],
+                "x-auth-token": response.json()["accessToken"],
+            },
+        )
+        # Set device ID assigned by ubank.
+        device_id = client.headers["x-device-id"] = response.json()["deviceId"]
+
+        # Generate a "hashed PIN", a random Base64 string.
+        hashed_pin = base64.standard_b64encode(secrets.token_bytes(66)).decode("utf-8")
+
+        # Authenticate with second factor: a security code sent to mobile.
+        otp_response = client.post(
+            url="https://api.ubank.com.au/app/v1/challenge/otp",
+            # Set parameters returned in previous response.
+            params={
+                "nonce": response.json()["nonce"],
+                "state": response.json()["state"],
+                "session": response.json()["session"],
+            },
+            json={
+                "deviceId": device_id,
+                "hashedPin": hashed_pin,
+                # Prompt interactively for security code.
+                "otpValue": input(
+                    f'Enter security code sent to {response.json()["maskedMobileNumber"]}: '
+                ),
+            },
+            # Set access and auth token headers from previous response.
+            headers={
+                "x-access-token": response.json()["accessToken"],
+                "x-auth-token": response.json()["accessToken"],
+            },
+        )
+        # Set access and auth token headers for future requests.
+        client.headers["x-access-token"] = client.headers["x-auth-token"] = (
+            otp_response.json()["accessToken"]
+        )
+
+        # Generate a secret.
+        secret = str(uuid.uuid4())
+
+        # Obtain a long life token. This is used to authenticate in the future.
+        response = client.post(
+            url="https://api.ubank.com.au/app/v1/long-life-token/generate",
+            json={"hashedPin": hashed_pin, "secret": secret},
+        )
+
+        # Clear this session's tokens.
+        client.request(
+            # Can't use client.delete() because bodies in DELETE requests have no
+            # defined semantics.
+            method="DELETE",
+            url="https://api.ubank.com.au/app/v1/sessions",
+            # Tokens were returned in /challenge/otp response.
+            json={
+                "accessToken": otp_response.json()["accessToken"],
+                "refreshToken": otp_response.json()["refreshToken"],
+                "sessionToken": otp_response.json()["sessionToken"],
+            },
+        )
+
+        # The enrolled device's information is required for future authentication.
+        return Device(
+            hardware_id=hardware_id,
+            device_id=device_id,
+            device_meta=device_meta,
+            hashed_pin=hashed_pin,
+            secret=secret,
+            auth_key=otp_response.json()["authKey"],
+            email=otp_response.json()["email"],
+            mobile_number=otp_response.json()["mobileNumber"],
+            user_id=otp_response.json()["userId"],
+            username=otp_response.json()["username"],
+            token=response.json()["token"],
+        )
 
 
 if __name__ == "__main__":
-    # Prints trusted cookie if run as module.
     parser = argparse.ArgumentParser(
-        description="Retrieves ubank trusted browser cookie"
+        description="Enrols new device with ubank. "
+        "You will be asked for your ubank password and secret code interactively.",
     )
-    parser.add_argument("username")
-    parser.add_argument("password")
+    parser.add_argument("username", help="ubank username")
+    parser.add_argument(
+        "-o",
+        "--output",
+        default="-",
+        type=argparse.FileType(mode="w"),
+        help="write JSON device credentials to file (default: write to stdout)",
+        dest="file",
+    )
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="displays httpx INFO logs"
+    )
     args = parser.parse_args()
-    with UbankClient() as ubank_client:
-        ubank_client.log_in_with_security_code(args.username, args.password)
-        print(ubank_client.get_trusted_cookie())
+    if args.verbose:
+        # Displays basic httpx request information.
+        logging.basicConfig(level=logging.INFO)
+    args.file.write(
+        enrol_device(args.username, password=getpass("Enter ubank password: ")).dumps()
+    )
